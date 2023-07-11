a27664e70b9ac7e6af1b39004aba9704412cb346
Sebastian Rittau <srittau@rittau.biz>
2023-07-11 14:46:40 +0200

    Limit progress to 40 ticks

--- app/src/main/java/org/rittau/mould/model/ProgressTrack.kt
+++ app/src/main/java/org/rittau/mould/model/ProgressTrack.kt
@@ -3,6 +3,8 @@ package org.rittau.mould.model
 import java.io.Serializable
 import java.util.UUID
 
+const val MAX_PROGRESS = 40
+
 enum class ProgressCompletion {
     InProgress, Completed, Failed
 }
@@ -16,10 +18,12 @@ data class ProgressTrack(
     var completion: ProgressCompletion = ProgressCompletion.InProgress,
 ) : Serializable
 
-fun addProgress(progress: Int, rank: ChallengeRank) = when (rank) {
-    ChallengeRank.Troublesome -> progress + 12
-    ChallengeRank.Dangerous -> progress + 8
-    ChallengeRank.Formidable -> progress + 4
-    ChallengeRank.Extreme -> progress + 2
-    ChallengeRank.Epic -> progress + 1
-}
+fun addProgress(progress: Int, rank: ChallengeRank) = minOf(
+    MAX_PROGRESS, when (rank) {
+        ChallengeRank.Troublesome -> progress + 12
+        ChallengeRank.Dangerous -> progress + 8
+        ChallengeRank.Formidable -> progress + 4
+        ChallengeRank.Extreme -> progress + 2
+        ChallengeRank.Epic -> progress + 1
+    }
+)
