08c70525e4373c6725dc49fea17a3e7a5f3960a0
Sebastian Rittau <srittau@rittau.biz>
2023-07-11 15:11:45 +0200

    Turn edit button into floating action button

--- app/src/main/java/org/rittau/mould/ui/NoteView.kt
+++ app/src/main/java/org/rittau/mould/ui/NoteView.kt
@@ -2,8 +2,10 @@ package org.rittau.mould.ui
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -11,6 +13,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -52,11 +55,6 @@ fun NoteView(
                     Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
                 }
             },
-            actions = {
-                IconButton(onClick = { onEdit(note) }) {
-                    Icon(Icons.Filled.Edit, contentDescription = "Edit")
-                }
-            },
             colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
         )
 
@@ -90,6 +88,12 @@ fun NoteView(
                 }
             }
             MarkdownText(note.text, style = MaterialTheme.typography.bodyLarge)
+        }
+    }
+
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomEnd) {
+        FloatingActionButton(onClick = { onEdit(note) }, modifier = Modifier.padding(16.dp)) {
+            Icon(Icons.Filled.Edit, contentDescription = "Edit")
         }
     }
 }
