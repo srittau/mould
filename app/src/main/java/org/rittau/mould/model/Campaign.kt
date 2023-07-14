0f6b78b6d396fd5c60bb6c07813faaae97a021cc
Sebastian Rittau <srittau@rittau.biz>
2023-07-14 23:33:20 +0200

    Fix crash

--- app/src/main/java/org/rittau/mould/model/Campaign.kt
+++ app/src/main/java/org/rittau/mould/model/Campaign.kt
@@ -1,7 +1,8 @@
 package org.rittau.mould.model
 
+import java.io.Serializable
 import java.util.UUID
 
-data class Campaign(val uuid: UUID, var name: String)
+data class Campaign(val uuid: UUID, var name: String) : Serializable
 
 data class LoadedCampaign(val character: Character, val progress: List<ProgressTrack>)
